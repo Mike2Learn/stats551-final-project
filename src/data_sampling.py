f94858af67b07d71bb40cbe235297b50535edfc3
Mike2Learn <yiweimai@umich.edu>
2023-11-14 20:12:03 -0500

    11.14

--- src/data_sampling.py
+++ src/data_sampling.py
@@ -1,3 +1,4 @@
+# Sampling 1% of total users for efficiently developing this project. Not necessary to run this script again.
 import pandas as pd
 import numpy as np
 
@@ -5,9 +6,11 @@ import numpy as np
 df = pd.read_csv('../data/UserBehavior.csv')
 df.columns = ['UserID', 'ItemID', 'ItemCategoryID', 'BehaviorType', 'TimeStamp']
 
-# Sample UserID
-sampled_UserID = np.random.choice(df['UserID'].unique(), size=10000)
-print(sampled_UserID)
+# Sample 1% users
+np.random.seed(551)
+sampled_UserID = np.random.choice(df['UserID'].unique(), size=int(0.01*len(df['UserID'].unique())))
 
-print(df[df['UserID'] in sampled_UserID])
+# Sample dataframe of sampled users
+sampled_df = df[df['UserID'].isin(sampled_UserID)]
 
+sampled_df.to_csv('../data/Sampled_UserBehavior.csv')
--- src/data_transformation.py
+++ src/data_transformation.py
@@ -1,7 +1,16 @@
+# Run this script to generate a pivot dataframe
 import pandas as pd
 
-df = pd.read_csv('../data/UserBehavior10000.csv').iloc[:,1:]
-df.columns = ['UserID', 'ItemID', 'ItemCategoryID', 'BehaviorType', 'TimeStamp']
-print(df)
 
+df = pd.read_csv('../data/Sampled_UserBehavior.csv').iloc[:, 1:]
+# df = pd.read_csv('../data/UserBehavior10000.csv').iloc[:, 1:]
+df.columns = ['UserID', 'ItemID', 'ItemCategoryID', 'BehaviorType', 'TimeStamp']
+
+# Pivot by behavior type, but dropping timestamp
+pivot_df = pd.pivot_table(df, values='BehaviorType', index=['UserID', 'ItemID', 'ItemCategoryID'], columns='BehaviorType', aggfunc='any')
+pivot_df.reset_index(inplace=True)
+
+pivot_df.to_csv('../data/UserBehavior-Without-Timestamp.csv')
+
+# TODO: how to transform while keeping timestamp?
 
