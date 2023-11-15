5c399d0be0a566b6c0567826a94f541f18945795
Mike2Learn <yiweimai@umich.edu>
2023-11-14 19:13:26 -0500

    1

---
+++ src/data_sampling.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv('data/UserBehavior.csv')
+df.columns = ['UserID', 'ItemID', 'ItemCategoryID', 'BehaviorType', 'TimeStamp']
+
+# Sample UserID
+sampled_UserID = np.random.choice(df['UserID'].unique(), size=10000)
+print(sampled_UserID)
+
+print(df[df['UserID'] in sampled_UserID])
+
